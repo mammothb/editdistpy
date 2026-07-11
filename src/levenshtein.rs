7bdccf7609d05bc3975db0808fa1b29c1a0f2e4b
mmb L <7960706+mammothb@users.noreply.github.com>
2026-07-11 14:01:22 +0800

    build: rust files

---
+++ src/levenshtein.rs
@@ -0,0 +1,3 @@
+pub fn distance(string_1: Option<&str>, string_2: Option<&str>, max_distance: i64) -> i32 {
+    0
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+mod damerau_osa;
+mod levenshtein;
+
+#[cfg(feature = "python")]
+mod editdistpy_rs;
+
+#[cfg(feature = "python")]
+pub use editdistpy_rs::_editdistpy;
